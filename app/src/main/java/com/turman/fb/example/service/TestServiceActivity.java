2e2d81b4f103fe073ea81d3c85303de58876d409
buobao <1039163450@qq.com>
2016-02-23 09:49:45 +0800

    service2

--- app/src/main/java/com/turman/fb/example/service/TestServiceActivity.java
+++ app/src/main/java/com/turman/fb/example/service/TestServiceActivity.java
@@ -1,10 +1,15 @@
 package com.turman.fb.example.service;
 
 import android.app.Activity;
+import android.app.Service;
+import android.content.ComponentName;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.turman.fb.R;
 
@@ -16,6 +21,24 @@ public class TestServiceActivity extends Activity {
     private Button start;
     private Button stop;
 
+    private Button bind;
+    private Button unbind;
+    private Button status;
+
+    TestService2.MyBinder binder;
+    private ServiceConnection conn = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            System.out.println("------Service Connected-------");
+            binder = (TestService2.MyBinder) service;
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            System.out.println("---------Service Disconnected---------");
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,7 +46,11 @@ public class TestServiceActivity extends Activity {
 
         start = (Button) findViewById(R.id.btn_start);
         stop = (Button) findViewById(R.id.btn_stop);
+        bind = (Button) findViewById(R.id.btn_bind);
+        unbind = (Button) findViewById(R.id.unbind);
+        status = (Button) findViewById(R.id.status);
 
+        //第一个实例
         final Intent intent = new Intent();
 
         intent.setAction("com.turman.fb.example.service.TEST_SERVICE1");
@@ -42,5 +69,64 @@ public class TestServiceActivity extends Activity {
             }
         });
 
+
+        //第二个实例
+        final Intent intent1 = new Intent();
+        intent.setAction("com.turman.fb.example.service.TEST_SERVICE2");
+        bind.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                bindService(intent, conn, Service.BIND_AUTO_CREATE);
+            }
+        });
+
+        unbind.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                unbindService(conn);
+            }
+        });
+
+        status.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"Service的count值为:"+binder.getCount(),Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

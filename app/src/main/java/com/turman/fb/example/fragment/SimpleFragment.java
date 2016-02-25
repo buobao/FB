1d112be4493312be7dfcc04678e8abed827eaba5
buobao <1039163450@qq.com>
2016-02-25 17:50:11 +0800

    simple fragment

--- app/src/main/java/com/turman/fb/example/fragment/SimpleFragment.java
+++ app/src/main/java/com/turman/fb/example/fragment/SimpleFragment.java
@@ -1,11 +1,13 @@
 package com.turman.fb.example.fragment;
 
+import android.animation.Animator;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.turman.fb.R;
 
@@ -13,6 +15,23 @@ import com.turman.fb.R;
  * Created by dqf on 2016/2/25.
  */
 public class SimpleFragment extends Fragment {
+
+    private String name = "step 2";
+
+    public interface CallBack{
+        public void getResult(String result);
+    }
+
+    public void getDate(CallBack callBack){
+        callBack.getResult(name);
+    }
+
+    @Override
+    public Animator onCreateAnimator(int transit, boolean enter, int nextAnim) {
+        Bundle bundle = getArguments();
+        Toast.makeText(getActivity(),"获取到的字符是："+bundle.getString("param"),Toast.LENGTH_SHORT).show();
+        return super.onCreateAnimator(transit, enter, nextAnim);
+    }
 
     //这里重写这个回调函数来给我们自定义的fragment绑定一个自定义的布局
     @Nullable
--- app/src/main/java/com/turman/fb/example/fragment/SimpleFragmentActivity.java
+++ app/src/main/java/com/turman/fb/example/fragment/SimpleFragmentActivity.java
@@ -6,6 +6,7 @@ import android.graphics.Point;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.Display;
+import android.widget.Toast;
 
 import com.turman.fb.R;
 
@@ -13,6 +14,8 @@ import com.turman.fb.R;
  * Created by dqf on 2016/2/25.
  */
 public class SimpleFragmentActivity extends Activity {
+
+    private String name = "step 1";
 
     @TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
     @Override
@@ -24,11 +27,21 @@ public class SimpleFragmentActivity extends Activity {
         Display dis = getWindowManager().getDefaultDisplay();
         Point size = new Point();
         dis.getSize(size);
+        Bundle bundle = new Bundle();
+        bundle.putString("param",name);
         if (size.x > size.y) {
             SimpleFragment f1 = new SimpleFragment();
+            f1.setArguments(bundle);
             getFragmentManager().beginTransaction().replace(R.id.frg,f1).commit();
+            f1.getDate(new SimpleFragment.CallBack() {
+                @Override
+                public void getResult(String result) {
+                    Toast.makeText(SimpleFragmentActivity.this,"返回的参数："+result,Toast.LENGTH_SHORT).show();
+                }
+            });
         } else {
             SimpleFragment1 f2 = new SimpleFragment1();
+            f2.setArguments(bundle);
             getFragmentManager().beginTransaction().replace(R.id.frg,f2).commit();
         }
     }
